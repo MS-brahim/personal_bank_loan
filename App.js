c10e258cc4deb805c3c53f00c39fbb1b7ab67912
BrahiMouissi <57224320+MS-brahim@users.noreply.github.com>
2021-04-26 15:45:29 +0000

    sign in temp

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import SignUpScreen from "./screens/SignUpScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your test!</Text>
+      <SignUpScreen/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/shared/ButtonShared.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native'
+const ButtonShared = (props) => {
+    return (
+        <View>            
+          <TouchableOpacity
+          style={styles.button}
+          onPress={props.onPress}
+          >
+          <Text style={{color:'white',fontSize:16}}>{props.text}</Text>
+        </TouchableOpacity>
+      </View>
+    )
+}
+
+export default ButtonShared
+
+const styles = StyleSheet.create({
+    button: {
+      alignItems: "center",
+      backgroundColor: "#ed3b45",
+      padding: 10,
+      marginTop: 45,
+      borderRadius: 30,
+      width:100,
+    }
+  });
