700c94d85881e2648ec0ef1eedc3bd81728ad5e9
Elmahdi KARBAL <56792446+mekarbal@users.noreply.github.com>
2021-04-27 09:43:00 +0000

    calcul

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import CreditCalcul from "./screens/CreditCalcul";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your test!</Text>
+      <CreditCalcul />
       <StatusBar style="auto" />
     </View>
   );
